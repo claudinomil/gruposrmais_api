93e8de55a7b4a5f1c626c6b63b9a71e082355f42
claudinomil <claudinomoraes@yahoo.com.br>
2026-09-07 11:00:28 -0300

    Missões 07/09/2026 2

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -58,7 +58,18 @@ class AppController extends Controller
         $registros = ClienteSistemaPreventivo::join('clientes', 'clientes.id', 'clientes_sistemas_preventivos.cliente_id')
             ->join('sistemas_preventivos', 'sistemas_preventivos.id', 'clientes_sistemas_preventivos.sistema_preventivo_id')
             ->join('medidas_seguranca', 'medidas_seguranca.id', 'sistemas_preventivos.medida_seguranca_id')
-            ->select('clientes_sistemas_preventivos.*', 'clientes.name as clienteName', 'sistemas_preventivos.name as sistemaPreventivoName', 'medidas_seguranca.name as medidaSegurancaName')
+            ->join('edificacoes_locais', 'edificacoes_locais.id', 'sistemas_preventivos.edificacao_local_id')
+            ->join('edificacoes_niveis', 'edificacoes_niveis.id', 'edificacoes_locais.edificacao_nivel_id')
+            ->join('edificacoes', 'edificacoes.id', 'edificacoes_niveis.edificacao_id')
+            ->select(
+                'clientes_sistemas_preventivos.*',
+                'clientes.name as clienteName',
+                'sistemas_preventivos.name as sistemaPreventivoName',
+                'medidas_seguranca.name as medidaSegurancaName',
+                'edificacoes_locais.name as edificacaoLocalName',
+                'edificacoes_niveis.name as edificacaoNivelName',
+                'edificacoes.name as edificacaoName'
+            )
             ->where('clientes_sistemas_preventivos.sistema_preventivo_numero', $sistema_preventivo_numero)
             ->first();
 
